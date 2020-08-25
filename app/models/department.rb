32ff983aad39e46fe02df59daf5af0ddec90320d
Hythan <hythan97@gmail.com>
2020-08-25 19:16:27 -0300

    add icon in destroy module, add validates

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,6 +2,10 @@ class Department < ApplicationRecord
     has_many :department_modules, dependent: :destroy
 
     validates :name, presence: true
+    validates :initials, presence: true, uniqueness: true
+    validates :local, presence: true
+    validates :email, presence: true, uniqueness: true
+
 
     def modules
         department_modules
