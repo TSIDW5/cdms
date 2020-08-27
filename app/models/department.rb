9134d9190b6016857757454917b24b4a99059351
Hythan <hythan97@gmail.com>
2020-08-27 14:38:30 -0300

    add regex for email and phone, changed locales links for department and department modules

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,13 +1,16 @@
 class Department < ApplicationRecord
+   
     has_many :department_modules, dependent: :destroy
 
     validates :name, presence: true
     validates :initials, presence: true, uniqueness: true
     validates :local, presence: true
-    validates :email, presence: true, uniqueness: true
-
-
+    validates :email, presence: true, uniqueness: true, format: {with: /\A\S+@.+\.\S+\z/, message: "inválido!"}
+    validates :phone, presence: true, format: {with: /\A[1-9]{2}9[0-9]{8}\z|\A[1-9]{2}[0-9]{8}\z/, message: "inválido!"}
+    
     def modules
         department_modules
     end
+
+    
 end
