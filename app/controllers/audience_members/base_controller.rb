b30d7367f8b284136c313da1c96082d904a71c0d
Matheus Mazepa <mfmazepa@hotmail.com>
2020-09-24 22:52:19 -0300

    Removing all refences admin on audience member

--- app/controllers/audience_members/base_controller.rb
+++ app/controllers/audience_members/base_controller.rb
@@ -1,3 +1,5 @@
 class AudienceMembers::BaseController < ActionController::Base
   layout 'layouts/audience_members/application'
+
+  add_breadcrumb I18n.t('views.breadcrumbs.home'), :audience_members_root_path
 end
