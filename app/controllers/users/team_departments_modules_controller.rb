767eb5f2e96953ab8b983b787e1ea14081b687c7
paulouskipsk <paulouskipsk@gmail.com>
2020-10-23 00:56:49 -0300

    fix view index staff-area-show-department-module

--- app/controllers/users/team_departments_modules_controller.rb
+++ app/controllers/users/team_departments_modules_controller.rb
@@ -1,9 +1,21 @@
 class Users::TeamDepartmentsModulesController < Users::BaseController
   def index     
-     @departmentsUser = DepartmentUser.includes(:department, :user).where(users: {id: current_user.id.to_s})
-     @modulesUser =DepartmentModuleUser.includes(:department_module, :user).where(users: {id: current_user.id.to_s})
+    @departmentsUser = DepartmentUser.includes(:department, :user).where(users: {id: current_user.id.to_s})
+    @modulesUser =DepartmentModuleUser.includes(:department_module, :user).where(users: {id: current_user.id.to_s})
 
-     puts(@departmentsUser)
-     puts(@modulesUser)
+    @departments = []
+    @departmentsUser.each do |dep_user|
+      department = Hash.new
+      modules = []
+      department.store("department", dep_user.department)      
+
+      @modulesUser.each do |mod_user|
+        if dep_user.department.id == mod_user.department_module.department_id
+          modules.push(mod_user.department_module)  
+        end        
+      end
+      department.store("modules", modules)
+      @departments.push(department)
+    end 
   end
 end
