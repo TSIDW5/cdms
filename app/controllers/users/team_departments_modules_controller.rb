a88fb013c125c1f4504fb807ee0ecfd37e55bef5
Lucas samuel <lukassamuka88@gmail.com>
2020-10-24 15:11:03 -0300

    added breadcrumps team-department-modules

--- app/controllers/users/team_departments_modules_controller.rb
+++ app/controllers/users/team_departments_modules_controller.rb
@@ -1,15 +1,22 @@
 class Users::TeamDepartmentsModulesController < Users::BaseController
+  before_action :breadcrumbs_team
+
   def index     
-      @departments = current_user.departments_and_modules
+    @departments = current_user.departments_and_modules
   end
 
   def show_department    
     validation_department_current_user
+    
+    add_breadcrumb I18n.t('views.department.links.show'), users_show_department_path(@department)
+
     @department_users = @department.department_users.includes(:user)
   end
 
   def show_module    
     validation_module_current_user
+    add_breadcrumb I18n.t('views.department_module.links.show'), users_show_module_path(@department_module)
+
     @module_users =  @department_module.department_module_users.includes(:user)
   end
 
@@ -30,6 +37,10 @@ class Users::TeamDepartmentsModulesController < Users::BaseController
     if (@department_module.department_module_users.where(user_id: current_user.id.to_s).empty?)
       redirect_to users_team_departments_modules_path
     end
+  end
+
+  def breadcrumbs_team
+    add_breadcrumb I18n.t('views.team.name.plural'), :users_team_departments_modules_path
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   authenticate :user do
     namespace :users do
       root to: 'dashboard#index'
-      #get 'team-departments-modules', controller: 'team_departments_modules', action: 'index', as: 'team_departments_modules'
       get 'team-departments-modules', to: 'team_departments_modules#index', action: :index
       get 'show-department/:id', to: 'team_departments_modules#show_department', action: :show_department, as: 'show_department'
       get 'show-module/:id', to: 'team_departments_modules#show_module', action: :show_module, as: 'show_module'
