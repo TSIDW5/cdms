4b4b7d21f761e0c115a9be9e19fc404d2ff0327c
paulouskipsk <paulouskipsk@gmail.com>
2020-10-24 14:46:43 -0300

    refactor index teamController

--- app/controllers/users/team_departments_modules_controller.rb
+++ app/controllers/users/team_departments_modules_controller.rb
@@ -1,33 +1,12 @@
 class Users::TeamDepartmentsModulesController < Users::BaseController
   def index     
-    @departmentsUser = DepartmentUser.includes(:department, :user).where(users: {id: current_user.id.to_s})
-    @modulesUser =DepartmentModuleUser.includes(:department_module, :user).where(users: {id: current_user.id.to_s})
-
-    @departments = []
-    @departmentsUser.each do |dep_user|
-      department = Hash.new
-      modules = Array.new 
-            
-      @modulesUser.each do |mod_user|
-        if dep_user.department.id == mod_user.department_module.department_id
-          mod = Hash.new
-          mod.store("role", mod_user.role)
-          mod.store('module', mod_user.department_module)
-          modules.push(mod)  
-        end        
-      end
-      department.store("department", dep_user.department) 
-      department.store("role", dep_user.role)
-      department.store("modules", modules)
-      @departments.push(department)
-    end 
+      @departments = current_user.departments_and_modules
   end
 
   def show_department    
     validation_department_current_user
     @department_users = @department.department_users.includes(:user)
   end
-
 
   def show_module    
     validation_module_current_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -55,4 +55,29 @@ class User < ApplicationRecord
   def self.search_non_admins(term)
     where(role_id: nil).where('unaccent(name) ILIKE unaccent(?)', "%#{term}%").order('name ASC')
   end
+
+  def departments_and_modules
+    departmentsUser = DepartmentUser.includes(:department, :user).where(users: {id: self.id.to_s})
+    modulesUser = DepartmentModuleUser.includes(:department_module, :user).where(users: {id: self.id.to_s})
+
+    departments = []
+    departmentsUser.each do |dep_user|
+      department = Hash.new
+      modules = Array.new 
+            
+      modulesUser.each do |mod_user|
+        if dep_user.department.id == mod_user.department_module.department_id
+          mod = Hash.new
+          mod.store("role", mod_user.role)
+          mod.store('module', mod_user.department_module)
+          modules.push(mod)  
+        end        
+      end
+      department.store("department", dep_user.department) 
+      department.store("role", dep_user.role)
+      department.store("modules", modules)
+      departments.push(department)
+    end
+    return departments 
+  end
 end
