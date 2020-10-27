055eb039c5d9c6fc6388c4bfc443157606937a41
Matheus Mazepa <mfmazepa@hotmail.com>
2020-10-26 22:14:24 -0300

    Fix test

--- test/system/admins/departments/create_test.rb
+++ test/system/admins/departments/create_test.rb
@@ -5,27 +5,33 @@ class CreateTest < ApplicationSystemTestCase
     setup do
       user = create(:user, :manager)
       login_as(user, as: :user)
-      @department = create(:department)
-
-      visit new_users_document_path
+      visit new_admins_department_path
     end
 
     should 'successfully' do
-      document = build(:document)
+      department = build(:department)
 
-      fill_in 'document_title', with: document.title
+      fill_in 'department_name', with: department.name
+      fill_in 'department_initials', with: department.initials
+      fill_in 'department_phone', with: department.phone
+      fill_in 'department_local', with: department.local
+      fill_in 'department_email', with: department.email
+      fill_in 'department_description', with: department.description
       submit_form
 
       flash_message = I18n.t('flash.actions.create.m', resource_name: Department.model_name.human)
       assert_selector('div.alert.alert-success', text: flash_message)
 
-      document = Document.last
+      department = Department.last
       within('table.table tbody') do
-        assert_selector "a[href='#{users_document_path(document)}']", text: document.id
-        assert_text document.title
+        assert_selector "a[href='#{admins_department_path(department)}']", text: department.initials
+        assert_text department.name
+        assert_text department.phone
+        assert_text department.local
+        assert_text department.email
 
-        assert_selector "a[href='#{users_preview_document_path(document)}']"
-        assert_selector "a[href='#{users_document_path(document)}'][data-method='delete']"
+        assert_selector "a[href='#{edit_admins_department_path(department)}']"
+        assert_selector "a[href='#{admins_department_path(department)}'][data-method='delete']"
       end
     end
 
@@ -34,8 +40,24 @@ class CreateTest < ApplicationSystemTestCase
 
       assert_selector('div.alert.alert-danger', text: I18n.t('flash.actions.errors'))
 
-      within('div.document_title') do
+      within('div.department_name') do
         assert_text(I18n.t('errors.messages.blank'))
+      end
+
+      within('div.department_initials') do
+        assert_text(I18n.t('errors.messages.blank'))
+      end
+
+      within('div.department_phone') do
+        assert_text(I18n.t('errors.messages.invalid'))
+      end
+
+      within('div.department_local') do
+        assert_text(I18n.t('errors.messages.blank'))
+      end
+
+      within('div.department_email') do
+        assert_text(I18n.t('errors.messages.invalid'))
       end
     end
   end
