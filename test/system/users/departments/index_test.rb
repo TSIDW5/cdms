d4fc76f8fe00c62626d15be817d2a1bb8debf818
Hythan <hythan97@gmail.com>
2020-10-30 21:26:11 -0300

    system tests

--- test/system/users/departments/index_test.rb
+++ test/system/users/departments/index_test.rb
@@ -3,8 +3,13 @@ require 'application_system_test_case'
 class IndexTest < ApplicationSystemTestCase
   context 'login as responsible user' do
     setup do
-      department_user = create(:department_user, :responsible)
+      @department_user = create(:department_user, :responsible)
       login_as(department_user.user)
+    end
+
+    should 'list all linked departments' do
+      visit users_departments_path
+      assert_selector "<th>#{User.human_attribute_name(:role)}<th>"
     end
   end
 end
