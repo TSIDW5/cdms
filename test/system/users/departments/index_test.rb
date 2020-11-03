b273f186ef0a1c3b659c71e5d1d0574b4ed86445
Hythan <hythan97@gmail.com>
2020-11-03 20:13:43 -0300

    undo test system user index

--- test/system/users/departments/index_test.rb
+++ test/system/users/departments/index_test.rb
@@ -4,12 +4,7 @@ class IndexTest < ApplicationSystemTestCase
   context 'login as responsible user' do
     setup do
       @department_user = create(:department_user, :responsible)
-      login_as(department_user.user)
-    end
-
-    should 'list all linked departments' do
-      visit users_departments_path
-      assert_selector "<th>#{User.human_attribute_name(:role)}<th>"
+      login_as(@department_user.user, as: :user)
     end
   end
 end
