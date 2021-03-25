59a8ac933cce323e5fed077f1656c068de222963
Hythan Oliveira <hythan97@gmail.com>
2021-03-25 19:22:38 -0300

    translate sidebar itens and mask phone field in new departments

--- test/system/admins/sidebar/sidebar_test.rb
+++ test/system/admins/sidebar/sidebar_test.rb
@@ -1,16 +1,10 @@
 require 'application_system_test_case'
 
-class DashboardTest < ApplicationSystemTestCase
-  context 'dashboard' do
+class SidebarTest < ApplicationSystemTestCase
+  context 'sidebar' do
     setup do
       user = create(:user, :manager)
       login_as(user, as: :user)
-    end
-
-    should 'display breadcrumbs' do
-      visit admins_root_path
-
-      assert_selector '.breadcrumb-item', text: I18n.t('views.breadcrumbs.home')
     end
 
     should 'display sidebar' do
