82e0e9788722e5a0af82a33bb91e5e8ce6e23aa1
Hythan Oliveira <hythan97@gmail.com>
2021-03-31 19:37:40 -0300

    corrections code climate

--- app/javascript/config/mask_phone.js
+++ app/javascript/config/mask_phone.js
@@ -1,12 +1,12 @@
-var behavior = function (val) {
-    return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+let behavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
 },
 options = {
-    onKeyPress: function (val, e, field, options) {
-        field.mask(behavior.apply({},arguments), options);
-    }
+  onKeyPress: function (val, e, field, options) {
+    field.mask(behavior.apply({}, arguments), options);
+  }
 };
 
 $(document).on('turbolinks:load', () => {
-    $('.phone_mask').mask(behavior, options);
+  $('.phone_mask').mask(behavior, options);
 });
