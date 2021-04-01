2add6854229adf3d6db0de69189140d4fc385277
Hythan Oliveira <hythan97@gmail.com>
2021-03-31 21:49:14 -0300

    fixed eslint issues

--- app/javascript/config/mask_phone.js
+++ app/javascript/config/mask_phone.js
@@ -1,10 +1,10 @@
-let behavior = function (val) {
-  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-},
-options = {
-  onKeyPress: function (val, e, field, options) {
-    field.mask(behavior.apply({}, arguments), options);
-  }
+const behavior = (val) => (val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009');
+
+const options = {
+  onKeyPress: (val, e, field, opts) => {
+    const args = [val, e, field, opts];
+    field.mask(behavior.apply({}, args), opts);
+  },
 };
 
 $(document).on('turbolinks:load', () => {
